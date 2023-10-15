be1b7728d795182152b449db8d799770d96735fa
vatiza <rakibur.mac@gmail.com>
2023-10-16 03:01:45 +0600

    inner html

--- javascript.js
+++ javascript.js
@@ -1,21 +1,29 @@
-console.log('hello');
-const liCollection = document.getElementsByTagName('li');
-// console.log(liCollection);
-for (const li of liCollection) {
-    console.log(li.innerText)
-}
-const allHeadingTag = document.getElementsByTagName('h1');
+// console.log('hello');
+// const liCollection = document.getElementsByTagName('li');
+// // console.log(liCollection);
+// for (const li of liCollection) {
+//     console.log(li.innerText)
+// }
+// const allHeadingTag = document.getElementsByTagName('h1');
 
-for (const h1 of allHeadingTag) {
-    console.log(h1);
-}
-const liName = document.getElementsByTagName('li');
-for (const allLi of liName) {
-    console.log(allLi.innerText)
-}
+// for (const h1 of allHeadingTag) {
+//     console.log(h1);
+// }
+// const liName = document.getElementsByTagName('li');
+// for (const allLi of liName) {
+//     console.log(allLi.innerText)
+// }
 
-const getId = document.getElementById('fruits-color')
-console.log(getId.innerText)
+// const getId = document.getElementById('fruits-color')
+// console.log(getId.innerText)
 
-const byClass = document.getElementsByClassName('fruits-name').innerText = 'bananna'
-console.log(byClass.innerText)
+// const byClass = document.getElementsByClassName('fruits-name').innerText = 'bananna'
+// console.log(byClass.innerText)
+
+// const someLi=document.querySelectorAll('.country')
+// console.log(someLi)
+const bgcolor = document.getElementById('title').style.textAlign = 'right';
+const setAttribure = document.getElementById('title');
+setAttribure.setAttribute('title', 'this header th');
+const chWel = document.getElementById('welcome');
+chWel.innerHTML='<h5>Hello Bangl</h5>';
