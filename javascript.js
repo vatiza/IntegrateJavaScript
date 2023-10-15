e01229584000e46fd066f05866a991ef9de9b0f9
vatiza <rakibur.mac@gmail.com>
2023-10-15 22:59:57 +0600

    getElemnetbyTagName

--- javascript.js
+++ javascript.js
@@ -1,2 +1,18 @@
 console.log('hello');
-console.log(document.body);
+const liCollection = document.getElementsByTagName('li');
+// console.log(liCollection);
+for (const li of liCollection) {
+    console.log(li.innerText)
+}
+const allHeadingTag = document.getElementsByTagName('h1');
+
+for (const h1 of allHeadingTag) {
+    console.log(h1);
+}
+const liName=document.getElementsByTagName('li');
+for (const allLi of liName){
+    console.log(allLi.innerText)
+}
+
+const getId=document.getElementById('fruits-color')
+console.log(getId.innerText)
