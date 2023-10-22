a1383b0481d7aba2a91e3004280c3d314e2e40c2
vatiza <rakibur.mac@gmail.com>
2023-10-22 11:32:21 +0600

    done this project

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     const dFiedAmount = document.getElementById('diposite-field');
     const newDepositAmount_string = dFiedAmount.value;
     const newDepositAmount_num = parseFloat(newDepositAmount_string);
+
+    if (isNaN(newDepositAmount_num)) {
+        alert('please input number');
+        return;
+    };
+
+
     // console.log(getDepositAmount);
     const totalDepostiAmount = document.getElementById('setDepositAmount');
 
@@ -24,20 +31,33 @@ document.getElementById('withdrow-btn').addEventListener('click', () => {
     const newwithdrowField = document.getElementById('withdrow-field');
     const newwithdrowFieldString = newwithdrowField.value;
     const newWithdrowFieldtoNum = parseFloat(newwithdrowFieldString);
+    if (isNaN(newWithdrowFieldtoNum)) {
+        alert('please input number');
+        return;
+    };
 
     const totalWithDrowElement = document.getElementById('setWithDrawAmount');
     const totalWithDrowElementString = totalWithDrowElement.innerText;
     const totalWithDrowElementToNumber = parseFloat(totalWithDrowElementString);
 
-    const sumTotalWithDrawAmmount = newWithdrowFieldtoNum + totalWithDrowElementToNumber;
-    // console.log(sumTotalWithDrawAmmount);
-    totalWithDrowElement.innerText = sumTotalWithDrawAmmount;
+
+
+
 
     const totalBalanceElement = document.getElementById('balance-total');
     const totalBalanceElementString = totalBalanceElement.innerText;
     const totalPreviousAmountNum = parseFloat(totalBalanceElementString);
+    if (newWithdrowFieldtoNum > totalPreviousAmountNum) {
+        alert('No amount');
+        return;
+    };
+    const sumTotalWithDrawAmmount = newWithdrowFieldtoNum + totalWithDrowElementToNumber;
+    totalWithDrowElement.innerText = sumTotalWithDrawAmmount;
+
+
     const setNewBalance = totalPreviousAmountNum - newWithdrowFieldtoNum;
     totalBalanceElement.innerText = setNewBalance;
+
 
     newwithdrowField.value = '';
 
