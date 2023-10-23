48c99ef979761731c3095b1b7d71687c5f17fead
vatiza <rakibur.mac@gmail.com>
2023-10-23 20:51:53 +0600

    triple calculation work

--- PracticeProblemInputField/javascript.js
+++ PracticeProblemInputField/javascript.js
@@ -11,3 +11,17 @@ document.getElementById('double-btn').addEventListener('click', function () {
     createNewLi.innerText = calculateToDouble;
     addNewElment.appendChild(createNewLi)
 })
+
+document.getElementById('triple-btn').addEventListener('click', function () {
+    const getNumberFromField = document.getElementById('input-field');
+    const getStringNumber = getNumberFromField.value;
+    const FieldStringConvetNumber = parseFloat(getStringNumber);
+
+
+    const calculateToDouble = FieldStringConvetNumber * 3;
+
+    const addNewElment = document.getElementById('outputli');
+    const createNewLi = document.createElement('li');
+    createNewLi.innerText = calculateToDouble;
+    addNewElment.appendChild(createNewLi)
+})
