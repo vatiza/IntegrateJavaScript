0f5c27db2108c4f0772043b49054c501db167c1f
vatiza <rakibur.mac@gmail.com>
2023-10-23 20:46:44 +0600

    double workd

---
+++ PracticeProblemInputField/javascript.js
@@ -0,0 +1,13 @@
+document.getElementById('double-btn').addEventListener('click', function () {
+    const getNumberFromField = document.getElementById('input-field');
+    const getStringNumber = getNumberFromField.value;
+    const FieldStringConvetNumber = parseFloat(getStringNumber);
+
+
+    const calculateToDouble = FieldStringConvetNumber * 2;
+
+    const addNewElment = document.getElementById('outputli');
+    const createNewLi = document.createElement('li');
+    createNewLi.innerText = calculateToDouble;
+    addNewElment.appendChild(createNewLi)
+})
